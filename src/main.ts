eb9d4a4b07283ceb6f9b96efadf42fc33f660724
AaronQ <aaron.quintana@myrenegade.net>
2026-03-13 04:30:43 -0400

    refactor: remove global API prefix setting from bootstrap function

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import { writeFileSync } from 'fs';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.setGlobalPrefix('api');
   app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
   app.enableCors();
 
